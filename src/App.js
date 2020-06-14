e17a7ffad6db9f4958089eccb0ce945f127b6e69
SereneMatt <serene1990@gmail.com>
2020-06-14 22:23:06 +0200

    Use withRouter

--- src/App.js
+++ src/App.js
@@ -1,30 +1,17 @@
 import React from 'react';
-import { Switch, Route, Link } from 'react-router-dom';
+import { Switch, Route } from 'react-router-dom';
 
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-  </div>
-);
-
-const TopicsList = props => {
-  return (
-    <div>
-      <h1>TOPIC LIST PAGE</h1>
-      <Link></Link>
-    </div>
-  )
-}
 function App() {
   return (
     <div>
       <Switch>
         <Route exact path='/' component={HomePage} />
-        <Route path='/hats' component={HatsPage} />
+        <Route path='/shop' component={ShopPage} />
       </Switch>
     </div>
   );
