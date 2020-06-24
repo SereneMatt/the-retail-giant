355f56e21971a0faf7b5a7a0004fa2073ed9e6b1
SereneMatt <serene1990@gmail.com>
2020-06-24 22:48:28 +0200

    Add header

--- src/App.js
+++ src/App.js
@@ -5,10 +5,13 @@ import './App.css';
 
 import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
+import Header from './components/header/header.component';
 
 function App() {
   return (
     <div>
+      {/* Always place header outside of switch. It always exists. */}
+      <Header />
       <Switch>
         <Route exact path='/' component={HomePage} />
         <Route path='/shop' component={ShopPage} />
