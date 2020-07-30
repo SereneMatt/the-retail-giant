56460849af6d1e777f6a2cbfdd390489b8066c4a
SereneMatt <serene1990@gmail.com>
2020-07-30 23:03:10 +0200

    sign in with google

--- src/components/sign-in/sign-in.component.jsx
+++ src/components/sign-in/sign-in.component.jsx
@@ -5,6 +5,8 @@ import './sign-in.styles.scss';
 import FormInput from '../form-input/form-input.component';
 import CustomButton from '../custom-button/custom-button.component';
 
+import { signInWithGoogle } from '../../firebase/firebase.utils';
+
 class SignIn extends React.Component {
   constructor(props) {
     super(props);
@@ -55,6 +57,7 @@ class SignIn extends React.Component {
           {/* Create <button></button> component for input type submit */}
           {/* Sign In is the children */}
           <CustomButton type='submit'>Sign In</CustomButton>
+          <CustomButton onClick={ signInWithGoogle }>Sign In With Google</CustomButton>
         </form>
       </div>
     )
