ac128e36983b7711e0ee3cae6add7ae425fde9e0
SereneMatt <serene1990@gmail.com>
2020-08-25 22:51:10 +0200

    Toggle states

--- src/redux/root-reducer.js
+++ src/redux/root-reducer.js
@@ -5,8 +5,10 @@
 import { combineReducers } from 'redux';
 
 import userReducer from './user/user.reducer';
+import cartReducer from './cart/cart.reducer';
 
 // One giant reducer
 export default combineReducers({
-  user: userReducer
+  user: userReducer,
+  cart: cartReducer
 });
