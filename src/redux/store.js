c5dfb6ee9a7a23f52b4fc6914e7da9621100be6b
SereneMatt <serene1990@gmail.com>
2021-01-06 21:12:44 +0100

    thunking

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,12 @@
 import { createStore, applyMiddleware } from 'redux';
 import { persistStore } from 'redux-persist';
 import logger from 'redux-logger';
+import thunk from 'redux-thunk';
 
 import rootReducer from './root-reducer';
 
 // middlewares between actions and reducers
-const middlewares = [];
+const middlewares = [thunk];
 
 // Log only in development environment
 if (process.env.NODE_ENV === 'development') {
